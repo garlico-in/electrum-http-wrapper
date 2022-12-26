e17229d70811107034069adc0ce8d616efed6e47
jorge <jorge@garlicoin.io>
2022-12-25 23:38:56 -0500

    Change imports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ElectrumClient = require('@samouraiwallet/electrum-client');
+import {ElectrumClient} from '@samouraiwallet/electrum-client';
 
 // Create a new Fastify server
 const fastify = require('fastify')({ logger: log })
