927aa907b19a7a75a1715e1f3053e0c70a0d12bc
jorge <jorge@garlicoin.io>
2022-12-27 19:36:37 -0500

    Fix maintianing multiple clients

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import fastify from 'fastify';
 import {ElectrumClient} from '@samouraiwallet/electrum-client';
 import garlicoinjs from 'garlicoinjs-lib';
+import os from 'os';
 
-// Create a new Fastify server
+let seedServer
+let client
+let clientMap
 const server = fastify({
   logger: {
     redact: ['req.headers.authorization'],
@@ -13,9 +16,12 @@ const server = fastify({
           id: request.id,
           method: request.method,
           url: request.url,
+          // TODO: remove headers with sensitive information
           headers: request.headers,
-          hostname: garlicoinjs.crypto.sha1(request.hostname),
-          remoteAddress: garlicoinjs.crypto.sha1(request.ip),
+          // Mask the hostname with a hash
+          hostname: garlicoinjs.crypto.sha1(request.hostname).toString('hex'),
+          // Mask the IP address with a hash
+          remoteAddress: garlicoinjs.crypto.sha1(request.ip).toString('hex'),
           remotePort: request.socket.remotePort
         }
       }
@@ -23,27 +29,102 @@ const server = fastify({
   }
 });
 
-// Create the initial Electrum client
-const client = new ElectrumClient(50002, 'electrumx.garlico.in', 'tls');
+async function initServer() {
 
-// Connect to the ElectrumX server
-try {
-  client.initElectrum({client: 'electrum-client-js', version: ['1.2', '1.4']}, {
-    retryPeriod: 5000,
-    maxRetry: 10,
-    pingPeriod: 5000,
-});
+  if (os.hostname() === 'batcave.garlico.in') {
+    console.info('Hostname is batcave.garlico.in');
+    seedServer = 'electrumx.garlico.in'
+  } else {
+    console.info('Hostname is not batcave.garlico.in');
+    seedServer = 'electrum.test.digital-assets.local'
+  }
 
-}catch(error){
-  console.log(error);
+  // Create the seed Electrum client
+  try{
+    client = new ElectrumClient(50002, seedServer, 'tls');
+  }catch(error){
+    console.error(error);
+  }
+
+  // Connect to the seed ElectrumX server
+  try {
+    client.initElectrum({client: 'electrum-client-js', version: ['1.2', '1.4']}, {
+      retryPeriod: 5000,
+      maxRetry: 10,
+      pingPeriod: 300000,
+  });
+
+  // Set onError handler
+  client.onerror = (error) => {
+    console.error(`Electrum client error: ${error}`);
+  }
+
+  }catch(error){
+    fastify.log.error(error);
+  }
+
 }
 
-const clientMap = await buildClientMap();
+// Rebuild the map of Electrum clients at set intervals
+async function rebuildClientMap(electrumClientsMap, interval) {
+  setInterval(async () => {
+    electrumClientsMap = await buildClientMap();
+  }, interval);
+}
+
+// Check the best Electrum server at set intervals
+async function checkBestElectrumServer(electrumClientsMap, interval) {
+  setInterval(async () => {
+
+    // Ping each server 3 times and take the average response time
+    let bestServer = null;
+    let bestResponseTime = Number.MAX_SAFE_INTEGER;
+  
+    // Loop through the Electrum clients
+    for (const [name, client] of electrumClientsMap) {
+      let totalResponseTime = 0;
+  
+      for (let i = 0; i < 3; i++) {
+        try {
+          const startTime = Date.now();
+          await client.server_ping();
+          const endTime = Date.now();
+          const responseTime = endTime - startTime;
+          totalResponseTime += responseTime;
+        } catch (error) {
+          console.error(`Error pinging Electrum server ${name}: ${error}`);
+          totalResponseTime += 1000;
+        }
+      }
+  
+      const averageResponseTime = totalResponseTime / 3;
+  
+      if (averageResponseTime < bestResponseTime) {
+        bestServer = name;
+        bestResponseTime = averageResponseTime;
+      }
+    }
+  
+    // Set the best Electrum client as the global client
+    fastify.log.info(`The best Electrum server is ${bestServer} with an average response time of ${bestResponseTime.toFixed(2)}ms`);
+    fastify.log.info('Changing the global Electrum client to ${bestServer}')
+
+    // Change the global Electrum client to the best server
+    try{
+      client = electrumClientsMap.get(bestServer);
+      fastify.log.info('Global Electrum client changed successfully.')
+    }catch(error){
+      fastify.log.error(error);
+      fastify.log.error('Failed to change global Electrum client.')
+    }
+  }, interval);
+}
+
 
 // A helper function to build a map of Electrum clients
 async function buildClientMap() {
   const tempClientMap = new Map();
-  tempClientMap.set('electrumx.garlico.in', client);
+  tempClientMap.set(seedServer, client);
 
   // Get a list of ElectrumX servers from the server
   const peerList = await client.serverPeers_subscribe();
@@ -64,36 +145,43 @@ async function buildClientMap() {
       }
 
     }catch(error){
-      console.log(error);
+      console.error(error);
     }
   }
 
-  // Connect to each ElectrumX server
+  // Connect to each ElectrumX server and add it to the map on success
   unconnectedClients.forEach((value, key) => {
     try {
 
       value.initElectrum({client: 'electrum-client-js', version: ['1.2', '1.4']}, {
         retryPeriod: 5000,
         maxRetry: 10,
-        pingPeriod: 5000,
+        pingPeriod: 300000,
       });
+
+      // Set onError handler
+      client.onerror = (error) => {
+        console.error(`Electrum client error: ${error}`);
+      }
+
       tempClientMap.set(key, value)
 
     } catch (error) {
-      console.log(`Failed to connect to server ${key}:50002`)
+      console.error(`Failed to connect to server ${key}:50002`)
     }
 
   });
 
-  console.log(`Connected to ${tempClientMap.size} servers.`)
+  console.info(`Connected to ${tempClientMap.size} servers.`)
   
-  // Return the map of good Electrum clients
+  // Return the map of goodElectrum clients
   return tempClientMap;
 }
 
 // A helper function to convert a garlicoin address to a scripthash
 function convertToScripthash(address) {
   
+  // Convert the address to a script using garlicoinjs
   let script = garlicoinjs.address.toOutputScript(address);
   let hash = garlicoinjs.crypto.sha256(script);
   return Buffer.from(hash.reverse()).toString('hex');
@@ -128,20 +216,10 @@ server.get('/api/GRLC/mainnet/address/:address/balance', async (request, reply) 
 
   // Connect to the ElectrumX server and send the transaction
   try {
-    
-    const responses = await Promise.all(clientMap.forEach(client => {
-      console.log('client:', client);
-      return client.blockchainScripthash_getBalance(scripthash);
-    }));
-
-    const response = responses.find(r => r.success);
-    if (response) {
-        reply.send(response);
-    } else {
-        throw 'Failed to get balance from any server.' 
-    }
+    const response = await client.blockchainScripthash_getBalance(scripthash);
 
     // Send the response from the ElectrumX server back to the client
+    reply.send(response);
   } catch (error) {
     reply.send(error);
   }
@@ -171,20 +249,24 @@ server.get('/api/GRLC/mainnet/peers', async (request, reply) => {
 
   // Connect to the ElectrumX server and get list of peers
   try {
-    const response = await client.serverPeers_subscribe();
+    const response = Object.keys(clientMap);
 
     // Send the response from the ElectrumX server back to the client
     reply.send(response);
   } catch (error) {
     reply.send(error);
+    fastify.log.error(error)
   }
 })
 
 // A route that responds to health checks
 server.get('/healthcheck', async (request, reply) => {
-
-  reply.code(200).send()
-
+  try{
+    reply.code(200).send()  
+  }catch(error){
+    reply.code(500).send()
+    fastify.log.error(error)
+  }
 })
 
 // Start the server
@@ -195,6 +277,15 @@ server.listen({ port: 3000, host: '0.0.0.0' }, async (error, address) => {
     process.exit(1);
   }
 
+  // Handles initial electrum client connection
+  initServer();
+
+  // Build a map of Electrum clients
+  clientMap = await buildClientMap();
+
+  // Check the best ElectrumX server every 60 seconds
+  checkBestElectrumServer(clientMap, 300000);
+  rebuildClientMap(clientMap, 3600000);
   server.log.info(`Server listening on ${address}`);
 
 });
